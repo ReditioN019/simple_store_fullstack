3cea7a708343f13fc296e933e700d00ee9a339c8
Sebastián <srojoa19@gmail.com>
2022-09-02 10:33:26 -0400

    optimización código frontend

--- client/js/app.js
+++ client/js/app.js
@@ -3,28 +3,22 @@ const productsList = document.querySelector('#products');
 const categoryList = document.querySelector("#category-list");
 const searchMessage = document.querySelector('#search-message');
 
-let products = [];
 
 window.addEventListener('DOMContentLoaded', async() => {
 
-    productsList.innerHTML = '<h1>Loading...</h1>'
+    renderLoadSpinner();
 
-    //!Obtengo las categorias
+    //!Carga las categorias
     const categoriesData = await loadCategories();
     renderCategories(categoriesData);//!Muestro las categorias en el boton
 
-
-
-    //!Cargo los productos
-    const data = await loadProducts();
-    //!Los productos (data) los asigno al array de productos que se mostraran en pantalla
-    products = data;
-    renderProducts(products);
+    //!Carga inicial de los productos
+    const productsData = await loadProducts();
+    renderProducts(productsData);
 });
 
 
 searchProduct.addEventListener('keyup', e => {
-
     fetch('http://localhost:4000/products', {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
@@ -35,10 +29,10 @@ searchProduct.addEventListener('keyup', e => {
     });
 })
 
-async function loadProducts(){
-    const resp = await fetch('http://localhost:4000/products');
-    return await resp.json();
-}
+const loadProducts = async() => await (await fetch('http://localhost:4000/products')).json();
+const loadCategories = async() => await (await fetch('http://localhost:4000/category')).json();
+const loadProductsByCategory = async(id) => await (await fetch(`http://localhost:4000/category/${id}`)).json(); 
+   
 
 const createProductsItems = products => products.map(product => {
     const { name, price, url_image } = product;
@@ -51,7 +45,6 @@ const createProductsItems = products => products.map(product => {
                 </div>
                 <div class="card-body">
                     <h6 class="card-title m-0 mb-2 text-success">${name}</h6>
-
                     <div class="d-flex justify-content-between align align-items-center">
                     <h3 class="mt-2 price">$${price}</h3>
                     <button 
@@ -67,42 +60,48 @@ const createProductsItems = products => products.map(product => {
     `;
 }).join('');
 
-function renderProducts(products){
-    const itemString = createProductsItems(products);
-    productsList.innerHTML = itemString
-}
 
-
-//!-----------------------------------------------------
-//!-----------------CATEGORIAS--------------------------
-//!-----------------------------------------------------
-
-async function loadCategories(){
-    const resp = await fetch('http://localhost:4000/category');
-    return await resp.json();
-    
-}
-
-
-const displayCategories = (categories) => categories.map((item) => {
-    return `
-        <li>
-            <a id="category" class="dropdown-item category"onClick="allProducts(${item.id})">${item.name}</a>   
-        </li>     
+const displayCategories = (categories) => categories.map((item) => (
+     `
+    <li>
+        <a id="category" class="dropdown-item category"onClick="productsByCategory(${item.id})">${item.name}</a>   
+    </li>     
     `
-}).join('');
+));
 
-
-async function allProducts(id){
-    const resp = await fetch(`http://localhost:4000/category/${id}`);
-    const data =  await resp.json();
-    products = data;
-    renderProducts(products);
+const productsByCategory = async(id) => {
+    const productsByCategory = await loadProductsByCategory(id);
+    renderProducts(productsByCategory);
 }
 
-function renderCategories(categories){
-    const itemString = displayCategories(categories);
-    categoryList.innerHTML = itemString;
+const allProducts = async() => {
+    const allProducts = await loadProducts();
+    renderProducts(allProducts);
 }
 
+//!===========================================================
+//!==================MOSTRAR EN PANTALLA======================
+//!===========================================================
 
+const renderProducts = (products) => {
+    const itemString = createProductsItems(products);
+    productsList.innerHTML = itemString;
+}
+
+const renderCategories = (categories) => {
+    const item = displayCategories(categories);
+    item.unshift(`
+        <li>
+            <a id="category" class="dropdown-item category" onClick="allProducts(0)">Todos</a>   
+        </li>  
+    `);
+    categoryList.innerHTML = item.join('');
+}
+
+const renderLoadSpinner = () => (
+    productsList.innerHTML = `
+        <div class="spinner-border" role="status">
+            <span class="visually-hidden">Loading...</span>
+        </div>
+    `
+)
